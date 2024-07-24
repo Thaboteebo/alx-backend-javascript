06b3acc2826527118261ebddd11052fd22713715
Thaboteebo <thaboteebo01@gmail.com>
2024-07-24 22:08:41 +0000

    ES6 Promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,12 @@
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
+
+export default function handleProfileSignup(firstName, last,Name, photo) {
+	return Promise
+		.allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
+		.then((results) => results.map(({ status, value, reason }) => ({
+			status,
+			value: status === 'fulfilled' ? value : reason.toString(),
+		})));
+
+}
